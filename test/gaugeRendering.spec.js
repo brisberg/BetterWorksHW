7a7655fe60f54beca439c60d95c62897dd2878cb
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-04 16:56:14 -0700

    Updated the tests to force finish the d3 animations before testing.

--- test/gaugeRendering.spec.js
+++ test/gaugeRendering.spec.js
@@ -8,6 +8,13 @@ describe('Gauge Directive Rendering', function () {
 
     var compile, mockBackend, rootScope;
 
+    function flushAllD3Transitions() {
+        var now = Date.now;
+        Date.now = function() { return Infinity; };
+        d3.timer.flush();
+        Date.now = now;
+    }
+
     beforeEach(inject(function ($compile, $rootScope) {
         compile = $compile;
         rootScope = $rootScope;
@@ -27,17 +34,18 @@ describe('Gauge Directive Rendering', function () {
 
         scope.$digest();
 
+        flushAllD3Transitions();
+
         expect(element.html().trim()).toEqual(
             '<svg width="140" height="140">' +
             '<g transform="translate(70,70)">' +
             '<path class="arc_inner" d="M0,53A53,53 0 1,1 0,-53A53,53 0 1,1 0,53M0,50A50,50 0 1,0 0,-50A50,50 0 1,0 0,50Z"></path>' +
-            '<path class="arc_outer_norm" d="M4.440892098500626e-15,-56.92099788303082A3,3 0 0,1 3.1578947368421098,-59.91683987687455A60,60 0 1,1 -48.897376500154785,34.771346988607014A3,3 0 0,1 -48.059988060084024,30.49979586267062L-48.059988060084024,30.49979586267062A3,3 0 0,1 -44.0076388501393,31.294212289746316A54,54 0 1,0 2.8421052631578987,-53.925155889187096A3,3 0 0,1 4.440892098500626e-15,-56.92099788303082Z"></path>' +
+            '<path class="arc_outer arc_outer_norm" d="M4.440892098500626e-15,-56.92099788303082A3,3 0 0,1 3.1578947368421098,-59.91683987687455A60,60 0 1,1 -48.897376500154785,34.771346988607014A3,3 0 0,1 -48.059988060084024,30.49979586267062L-48.059988060084024,30.49979586267062A3,3 0 0,1 -44.0076388501393,31.294212289746316A54,54 0 1,0 2.8421052631578987,-53.925155889187096A3,3 0 0,1 4.440892098500626e-15,-56.92099788303082Z" style="fill: #78c000;"></path>' +
             '<path class="circle_center" d="M0,43A43,43 0 1,1 0,-43A43,43 0 1,1 0,43Z"></path>' +
             '<text transform="translate(0,-0)" style="text-anchor: middle;">' +
-            '<tspan class="progress_number">66</tspan>' +
-            '<tspan class="progress_percent">%</tspan>' +
+            '<tspan class="progress_number">66</tspan><tspan class="progress_percent">%</tspan>' +
             '</text>' +
-            '<text class="progress_label" transform="translate(0,15)" style="text-anchor: middle;">Progress</text>' +
+            '<text class="progress_label" font-size="12px" transform="translate(0,15)" style="text-anchor: middle;">Progress</text>' +
             '</g>' +
             '</svg>');
     });
