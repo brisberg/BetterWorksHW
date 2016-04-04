40e200102d9131cb749a9bb990bae0ae32b9a936
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-04 16:30:04 -0700

    Added basic animation to the gauge arcs

--- src/app/gauge/gauge-directive.js
+++ src/app/gauge/gauge-directive.js
@@ -41,7 +41,7 @@ angular.module('betterworkshw')
                         var arcInnerFrac = metric.expected;
                         var arcOuterFrac = metric.current;
                         var progressFrac = metric.current / metric.expected;
-                        var progressLabel = (100 * metric.current).toFixed();
+                        var progressLabel = (100 * metric.current).toFixed() + "";
                         var outerColor = "arc_outer_norm";
 
                         if (progressFrac < 0.25) {
@@ -49,6 +49,32 @@ angular.module('betterworkshw')
                         } else if (progressFrac < 0.5) {
                             outerColor = "arc_outer_warn"
                         }
+
+                        // set up animation functions
+                        var drawArc = d3.svg.arc()
+                            .innerRadius(function(d) {
+                                return d.innerRadius;
+                            })
+                            .outerRadius(function(d) {
+                                return d.outerRadius;
+                            })
+                            .startAngle(0)
+                            .endAngle(function(d) {
+                                return d.endAngle;
+                            })
+                            .cornerRadius(10);
+
+                        var arcTween = function(transition, newAngle) {
+                            transition.attrTween("d", function(d) {
+                                var interpolate = d3.interpolate(d.endAngle, newAngle);
+
+                                return function(t) {
+                                    d.endAngle = interpolate(t);
+                                    return drawArc(d);
+                                };
+                            });
+                        }
+
 
                         if (typeof(d3) !== "undefined") { // Make sure d3 library is available
                             d3.select($element[0]).selectAll("*").remove();
@@ -60,33 +86,31 @@ angular.module('betterworkshw')
                                 .append("g")
                                 .attr("transform", "translate(70,70)");
 
-                            var arc_inner = d3.svg.arc()
-                                .innerRadius(50)
-                                .outerRadius(53)
-                                .startAngle(0)
-                                .endAngle(arcInnerFrac * 2 * Math.PI)
-                                .cornerRadius(10);
+                            var arc_inner = drawArc({endAngle:0, innerRadius:50, outerRadius:53});
 
                             svg.append("path")
+                                .datum({endAngle:0, innerRadius:50, outerRadius:53})
                                 .attr("class", "arc_inner")
-                                .attr("d", arc_inner);
+                                .attr("d", arc_inner)
+                                .transition()
+                                .duration(3000)
+                                .call(arcTween, (arcOuterFrac * 2 * Math.PI));
 
-                            var arc_outer = d3.svg.arc()
-                                .innerRadius(54)
-                                .outerRadius(60)
-                                .startAngle(0)
-                                .endAngle(arcOuterFrac * 2 * Math.PI)
-                                .cornerRadius(10);
+                            var arc_outer = drawArc({endAngle:0, innerRadius:54, outerRadius:60});
 
                             svg.append("path")
+                                .datum({endAngle:0, innerRadius:54, outerRadius:60})
                                 .attr("class", outerColor)
-                                .attr("d", arc_outer);
+                                .attr("d", arc_outer)
+                                .transition()
+                                .duration(3000)
+                                .call(arcTween, (arcOuterFrac * 2 * Math.PI));
 
                             var circle_center = d3.svg.arc()
                                 .innerRadius(0)
                                 .outerRadius(43)
                                 .startAngle(0)
-                                .endAngle(2 * Math.PI)
+                                .endAngle(2 * Math.PI);
 
                             svg.append("path")
                                 .attr("class", "circle_center")
@@ -107,6 +131,7 @@ angular.module('betterworkshw')
                             svg.append("text")
                                 .style("text-anchor", "middle")
                                 .attr("class", "progress_label")
+                                .attr("font-size", "12px")
                                 .text("Progress")
                                 .attr("transform", "translate(0,15)");
                         }
