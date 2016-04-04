23e71c79be020a63381634eafa2bea703b62c7ce
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-04 16:34:12 -0700

    Refactored the animation methods and watches to only trigger animations instead of repaints on data changes and smoothly animates the arcs.

--- src/app/gauge/gauge-directive.js
+++ src/app/gauge/gauge-directive.js
@@ -11,14 +11,14 @@ angular.module('betterworkshw')
                 var metric = $scope.metric;
 
                 $scope.$watch("metric.current", function () {
-                    repaint(metric);
+                    update(metric);
                 });
 
                 $scope.$watch("metric.expected", function () {
-                    repaint(metric);
+                    update(metric);
                 });
 
-                function repaint(metric) {
+                function render(metric) {
                     if (typeof(metric.expected) !== "number" || metric.expected < 0.0 || metric.expected > 1.0) {
                         // Based on http://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
                         // If the data is wrong, remove all SVG elements and place an error text.
@@ -50,32 +50,6 @@ angular.module('betterworkshw')
                             outerColor = "arc_outer_warn"
                         }
 
-                        // set up animation functions
-                        var drawArc = d3.svg.arc()
-                            .innerRadius(function(d) {
-                                return d.innerRadius;
-                            })
-                            .outerRadius(function(d) {
-                                return d.outerRadius;
-                            })
-                            .startAngle(0)
-                            .endAngle(function(d) {
-                                return d.endAngle;
-                            })
-                            .cornerRadius(10);
-
-                        var arcTween = function(transition, newAngle) {
-                            transition.attrTween("d", function(d) {
-                                var interpolate = d3.interpolate(d.endAngle, newAngle);
-
-                                return function(t) {
-                                    d.endAngle = interpolate(t);
-                                    return drawArc(d);
-                                };
-                            });
-                        }
-
-
                         if (typeof(d3) !== "undefined") { // Make sure d3 library is available
                             d3.select($element[0]).selectAll("*").remove();
 
@@ -92,19 +66,13 @@ angular.module('betterworkshw')
                                 .datum({endAngle:0, innerRadius:50, outerRadius:53})
                                 .attr("class", "arc_inner")
                                 .attr("d", arc_inner)
-                                .transition()
-                                .duration(1200)
-                                .call(arcTween, (arcInnerFrac * 2 * Math.PI));
 
                             var arc_outer = drawArc({endAngle:0, innerRadius:54, outerRadius:60});
 
                             svg.append("path")
                                 .datum({endAngle:0, innerRadius:54, outerRadius:60})
-                                .attr("class", outerColor)
+                                .attr("class", "arc_outer " + outerColor)
                                 .attr("d", arc_outer)
-                                .transition()
-                                .duration(1200)
-                                .call(arcTween, (arcOuterFrac * 2 * Math.PI));
 
                             var circle_center = d3.svg.arc()
                                 .innerRadius(0)
@@ -122,7 +90,7 @@ angular.module('betterworkshw')
 
                             upperText.append("tspan")
                                 .attr("class", "progress_number")
-                                .text(progressLabel);
+                                .text("+");
 
                             upperText.append("tspan")
                                 .attr("class", "progress_percent")
@@ -137,6 +105,50 @@ angular.module('betterworkshw')
                         }
                     }
                 }
+
+                function update(metric) {
+                    var arcInner = d3.select($element[0]).selectAll(".arc_inner");
+                    var arcOuter = d3.select($element[0]).selectAll(".arc_outer");
+                    var progressNumber = d3.select($element[0]).selectAll(".progress_number");
+
+                    arcInner.transition()
+                        .duration(1200)
+                        .call(arcTween, (metric.expected * 2 * Math.PI));
+                    arcOuter.transition()
+                        .duration(1200)
+                        .call(arcTween, (metric.current * 2 * Math.PI));
+                    progressNumber.text((100 * metric.current).toFixed() + "")
+                }
+
+                // set up animation functions
+                var drawArc = d3.svg.arc()
+                    .innerRadius(function(d) {
+                        return d.innerRadius;
+                    })
+                    .outerRadius(function(d) {
+                        return d.outerRadius;
+                    })
+                    .startAngle(0)
+                    .endAngle(function(d) {
+                        return d.endAngle;
+                    })
+                    .cornerRadius(10);
+
+                function arcTween(transition, newAngle) {
+                    transition.attrTween("d", function(d) {
+                        var interpolate = d3.interpolate(d.endAngle, newAngle);
+
+                        return function(t) {
+                            d.endAngle = interpolate(t);
+                            return drawArc(d);
+                        };
+                    });
+                }
+
+
+                // set up initial display.
+                render(metric);
+                update(metric);
             }
         }
     }]
