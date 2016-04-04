607b6cec668596f9357c68ba9972411fd92dae56
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-03 18:14:47 -0700

    Minor change to test data

--- src/app/main/main-controller.js
+++ src/app/main/main-controller.js
@@ -9,7 +9,7 @@ angular.module('betterworkshw')
             {name: 'Quarterly reports filed', expected: 0.45, current: 0.10},
             {name: 'Summoning C\'thun', expected: 1.00, current: 0.92},
             {name: 'Version 2.4 release', expected: 0.64, current: 0.40},
-            {name: 'Pencils sharpened', expected: 0.64, current: 0.40},
-            {name: 'Potato chips fabricated', expected: 0.70, current: 0.65}
+            {name: 'Potato chips fabricated', expected: 0.70, current: 0.65},
+            {name: 'Pencils sharpened', expected: 0.71, current: 0.30}
         ];
     });
