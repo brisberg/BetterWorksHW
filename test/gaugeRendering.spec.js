f2e07680e4a602eb4d3eeda77437efb93b997187
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-04 00:13:49 -0700

    Updated the gauge test

--- test/gaugeRendering.spec.js
+++ test/gaugeRendering.spec.js
@@ -33,8 +33,11 @@ describe('Gauge Directive Rendering', function () {
             '<path class="arc_inner" d="M0,53A53,53 0 1,1 0,-53A53,53 0 1,1 0,53M0,50A50,50 0 1,0 0,-50A50,50 0 1,0 0,50Z"></path>' +
             '<path class="arc_outer_norm" d="M4.440892098500626e-15,-56.92099788303082A3,3 0 0,1 3.1578947368421098,-59.91683987687455A60,60 0 1,1 -48.897376500154785,34.771346988607014A3,3 0 0,1 -48.059988060084024,30.49979586267062L-48.059988060084024,30.49979586267062A3,3 0 0,1 -44.0076388501393,31.294212289746316A54,54 0 1,0 2.8421052631578987,-53.925155889187096A3,3 0 0,1 4.440892098500626e-15,-56.92099788303082Z"></path>' +
             '<path class="circle_center" d="M0,43A43,43 0 1,1 0,-43A43,43 0 1,1 0,43Z"></path>' +
-            '<text font-size="20px" transform="translate(0,-2)" style="text-anchor: middle;">66%</text>' +
-            '<text font-size="12px" font-style="oblique" transform="translate(0,15)" style="text-anchor: middle;">Progress</text>' +
+            '<text transform="translate(0,-0)" style="text-anchor: middle;">' +
+            '<tspan class="progress_number">66</tspan>' +
+            '<tspan class="progress_percent">%</tspan>' +
+            '</text>' +
+            '<text class="progress_label" transform="translate(0,15)" style="text-anchor: middle;">Progress</text>' +
             '</g>' +
             '</svg>');
     });
