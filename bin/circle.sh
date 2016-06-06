5e8ce050709f20fd5798ac3f57b1ac7d34621e1a
Brandon Risberg <brandon@betterworks.com>
2016-06-06 14:29:45 -0700

    changing deploy name

--- bin/circle.sh
+++ bin/circle.sh
@@ -1,4 +1,4 @@
-git config --global user.email "brandon.risberg@gmail.com"
-git config --global user.name "brandon risberg"
+git config --global user.email "example@email.com"
+git config --global user.name "circletestingcommit"
 
 node ./node_modules/gulp/bin/gulp.js deploy
